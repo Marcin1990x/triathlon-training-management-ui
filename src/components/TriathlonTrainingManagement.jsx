31934adba2d0df3e50f2b247a90e10bd1b2d2961
Marcin1990x <marcin.konecki21@gmail.com>
2024-02-08 18:19:21 +0100

    First commit.

---
+++ src/components/TriathlonTrainingManagement.jsx
@@ -0,0 +1,7 @@
+export default function TriathlonTrainingManagement() {
+    return(
+        <div className="TriathlonTrainingManager">
+            <h1>Welcome in my new app !</h1>
+        </div>
+    )
+}
